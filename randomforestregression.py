241de4975a821211301e14a155fae62f7a79adfb
Nodar <nsotkilava@outlook.com>
2021-05-16 22:41:41 -0400

    stuff

--- randomforestregression.py
+++ randomforestregression.py
@@ -1,29 +1,26 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, recall_score, f1_score
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
 
 
-waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Sitesbycords-fall.csv", sep=r'\s*,\s*')
-#waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Sitesbycords-winter.csv", sep=r'\s*,\s*')
-#waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Sitesbycords-spring.csv", sep=r'\s*,\s*')
-#waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Sitesbycords-summer.csv", sep=r'\s*,\s*')
+#waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Fall\\fall_2017.csv", sep=r'\s*,\s*')
+#waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Spring\\spring_2017.csv", sep=r'\s*,\s*')
+#waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Winter\\winter_2017.csv", sep=r'\s*,\s*')
+waterAverage = pd.read_csv("D:\Developer\water_ml\Data\Summer\\summer_2017.csv", sep=r'\s*,\s*')
 
+y = np.array(waterAverage['range'].dropna())
+X = np.array(waterAverage[['lat','long']].dropna())
 
-#y = np.array(waterAverage['average'])
-y = np.array(waterAverage['NSE_state'])
-X = np.array(waterAverage[['dec_lat_va','dec_long_va']])
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 
 
-
-#clf = RandomForestRegressor(n_estimators=5000,  random_state=100)
 clf = RandomForestClassifier(n_estimators=5000,  random_state=100)
 
 
@@ -32,10 +29,6 @@ clf.fit(X_train, y_train)
 y_predict = clf.predict(X_test)
 
 
-print('Accuracy: %.2f' 
-      % metrics.accuracy_score(y_predict,y_test))
-print('Mean squared error: %.2f'
-      % metrics.mean_squared_error(y_test, y_predict))
-print(metrics.classification_report(y_predict,y_test))
-# print('Coefficient of determination: %.2f'
-#       % metrics.r2_score(y_test, y_predict))
+print('f1-score: %.2f' 
+      % metrics.f1_score(y_test, y_predict, average = "micro"))
+#print(metrics.classification_report(y_predict,y_test))
